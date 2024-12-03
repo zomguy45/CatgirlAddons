771667ed9820676bd7c0f876f6299b43dcbfc00c
TurtleOnFire2 <ijon.lorenz@gmail.com>
2024-12-03 13:26:26 +0100

    Started Autoleap (I cant test shit...)

--- src/main/kotlin/catgirlroutes/module/impl/dungeons/AutoLeap.kt
+++ src/main/kotlin/catgirlroutes/module/impl/dungeons/AutoLeap.kt
@@ -4,34 +4,71 @@ import catgirlroutes.CatgirlRoutes.Companion.mc
 import catgirlroutes.events.impl.PacketReceiveEvent
 import catgirlroutes.module.Category
 import catgirlroutes.module.Module
+import catgirlroutes.module.settings.impl.BooleanSetting
+import catgirlroutes.module.settings.impl.StringSelectorSetting
 import catgirlroutes.utils.ChatUtils.modMessage
-import me.odinmain.utils.sidebarLines
+import catgirlroutes.utils.sidebarLines
 import me.odinmain.utils.skyblock.unformattedName
 import net.minecraft.network.play.client.C0EPacketClickWindow
 import net.minecraft.network.play.server.S2DPacketOpenWindow
 import net.minecraft.network.play.server.S2FPacketSetSlot
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
+import kotlin.text.Regex
+
+//TODO: Change the lines that are being checked to the right ones.
+//      Add a way to select the player names.
+//      Make the triggers for the auto leap.
+//      Add S2D cancel.
+//      Add fail safes.
 
 object AutoLeap : Module(
     name = "Auto Leap",
     category = Category.DUNGEON
 ) {
+    private val usePlayerName = BooleanSetting("Use playernames",false)
+    private val s1target = StringSelectorSetting("Target S1", "A", arrayListOf("A", "B", "M", "H", "T"))
+    private val s2target = StringSelectorSetting("Target S2", "A", arrayListOf("A", "B", "M", "H", "T"))
+    private val s3target = StringSelectorSetting("Target S3", "A", arrayListOf("A", "B", "M", "H", "T"))
+    private val s4target = StringSelectorSetting("Target S4", "A", arrayListOf("A", "B", "M", "H", "T"))
+    init {
+        this.addSettings(
+            usePlayerName,
+            s1target,
+            s2target,
+            s3target,
+            s4target,
+        )
+    }
     var cwid = -1
     var awaitingLeap = false
-    var playerName = "SERENITYISSBLACK"
-    var playerList = mutableListOf<String>()
+    var currentTarget = ""
+    var playerList = mutableListOf<Map<String, String>>()
 
-    override fun onKeyBind() {
-        leap("test")
-    }
-    fun leap(playerName: String) {
+    fun leap(target: String) {
         var i = 0
-        modMessage("Debug")
         playerList.clear()
-        while (i < 5){
-            i += 1
-            playerList.add(sidebarLines[i])
-            modMessage(sidebarLines.get(i))
+        if (usePlayerName.enabled) {
+            currentTarget = target
+            awaitingLeap = true
+        } else {
+            while (i < 5){
+                val regex = Regex("""\[([A-Z])\]\s+(.+?)\s""")
+                val match = regex.find(sidebarLines[i])
+                if (match != null) {
+                    val playerClass = match.groupValues[1]
+                    val playerName = match.groupValues[2]
+                    playerList.add(mapOf("playerClass" to playerClass, "playerName" to playerName))
+                }
+                i += 1
+            }
+
+            for (player in playerList) {
+                if (player["playerClass"] == target) {
+                    currentTarget == player["playerName"]
+                    modMessage(currentTarget)
+                    awaitingLeap = true
+                }
+            }
         }
     }
 
@@ -41,7 +78,7 @@ object AutoLeap : Module(
         if (!awaitingLeap) return
         val item = event.packet.func_149174_e()
         val slot = event.packet.func_149173_d()
-        if (item.unformattedName.startsWith(playerName)) {
+        if (item.unformattedName == currentTarget) {
             click(slot)
             awaitingLeap = false
         }
