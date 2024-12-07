4ec16d3d64de76c63d9180ac9b3bce8aaa994884
WompWatr <markvartsk@gmail.com>
2024-12-07 14:14:11 +0200

    Blink improvements entityaura and test command

--- src/main/kotlin/catgirlroutes/utils/EntityAura.kt
+++ src/main/kotlin/catgirlroutes/utils/EntityAura.kt
@@ -1,15 +1,21 @@
 package catgirlroutes.utils
 
+import catgirlroutes.utils.ClientListener.scheduleTask
 import catgirlroutes.utils.PacketUtils.sendPacket
 import catgirlroutes.utils.Utils.distanceToPlayer
+import catgirlroutes.utils.render.WorldRenderUtils.drawBoxByEntity
 import net.minecraft.entity.Entity
 import net.minecraft.network.play.client.C02PacketUseEntity
+import net.minecraftforge.client.event.RenderWorldLastEvent
+import net.minecraftforge.event.world.WorldEvent
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
 import net.minecraftforge.fml.common.gameevent.TickEvent
+import java.awt.Color
 
 object EntityAura {
 
     val entityArray = mutableListOf<Entity>()
+    val recentClicks = mutableListOf<Entity>()
 
     @SubscribeEvent
     fun onTick(event: TickEvent.ClientTickEvent) {
@@ -17,10 +23,24 @@ object EntityAura {
         entityArray.forEach{entity ->
             if (distanceToPlayer(entity.posX, entity.posY, entity.posZ) < 4) {
                 sendPacket(C02PacketUseEntity(entity, C02PacketUseEntity.Action.INTERACT))
+                recentClicks.add(entity)
+                scheduleTask(2) {recentClicks.remove(entity)}
                 entityArray.remove(entity)
                 return
             }
         }
+    }
+
+    @SubscribeEvent
+    fun onRenderWorld(event: RenderWorldLastEvent) {
+        recentClicks.forEach{entity ->
+            drawBoxByEntity(entity, Color.PINK, entity.width, entity.height)
+        }
+    }
+
+    @SubscribeEvent
+    fun onWorldUnload(event: WorldEvent.Unload) {
+        entityArray.clear()
     }
 }
 
