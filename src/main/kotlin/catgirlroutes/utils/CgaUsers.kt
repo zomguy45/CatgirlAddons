f18f6d0b600cc250d4f70f18fb50289484bf141d
pirog <mihail.larin00@gmail.com>
2024-12-24 10:30:32 +0300

    other cga users can see PlayerSize size

---
+++ src/main/kotlin/catgirlroutes/utils/CgaUsers.kt
@@ -0,0 +1,58 @@
+package catgirlroutes.utils
+
+import catgirlroutes.CatgirlRoutes.Companion.scope
+import com.google.gson.*
+import kotlinx.coroutines.runBlocking
+import java.io.StringReader
+import java.lang.reflect.Type
+
+object CgaUsers {
+
+    var users: HashMap<String, CgaUser> = HashMap()
+    data class CgaUser(val xScale: Float = 1f, val yScale: Float = 1f, val zScale: Float = 1f)
+    data class UserData(val username: String, val uuid: String, val permission: Int, val dimensions: Triple<Float, Float, Float>)
+
+    class UserDeserializer : JsonDeserializer<UserData> {
+        override fun deserialize(
+            json: JsonElement?,
+            typeOfT: Type?,
+            context: JsonDeserializationContext?
+        ): UserData {
+            val jsonObject = json?.asJsonObject
+            val name = jsonObject?.get("name")?.asString
+            val uuid = jsonObject?.get("uuid")?.asString
+            val permission = jsonObject?.get("permission")?.asInt
+            val dimensionsJsonObject = JsonParser().parse(jsonObject?.get("dimensions")?.asString?.let { StringReader(it) })?.asJsonObject
+            val sizeTriple = Triple(
+                dimensionsJsonObject?.get("x")?.asFloat ?: 0f,
+                dimensionsJsonObject?.get("y")?.asFloat ?: 0f,
+                dimensionsJsonObject?.get("z")?.asFloat ?: 0f
+            )
+
+            return UserData(name ?: "", uuid ?: "", permission ?: 1, sizeTriple)
+        }
+    }
+
+    private fun convertDecimalToNumber(s: String): String {
+        val pattern = Regex("""Decimal\('(-?\d+(?:\.\d+)?)'\)""")
+
+        return s.replace(pattern) { match -> match.groupValues[1] }
+    }
+
+    fun updateUsers(): HashMap<String, CgaUser> {
+        runBlocking(scope.coroutineContext) {
+            val data = convertDecimalToNumber(getDataFromServer()).ifEmpty { return@runBlocking }
+            val gson = GsonBuilder().registerTypeAdapter(UserData::class.java, UserDeserializer())?.create() ?: return@runBlocking
+            gson.fromJson(data, Array<UserData>::class.java).forEach {
+                users[it.username] = CgaUser(it.dimensions.first, it.dimensions.second, it.dimensions.third)
+            }
+        }
+        println(users)
+        return users
+    }
+
+    init {
+        updateUsers()
+    }
+
+}
--- src/main/kotlin/catgirlroutes/utils/WebUtils.kt
+++ src/main/kotlin/catgirlroutes/utils/WebUtils.kt
@@ -1,7 +1,9 @@
 package catgirlroutes.utils
 
 import com.google.gson.JsonParser
-import kotlinx.coroutines.withTimeoutOrNull
+import kotlinx.coroutines.*
+import java.io.*
+import java.net.HttpURLConnection
 import java.net.URL
 
 suspend fun hasBonusPaulScore(): Boolean = withTimeoutOrNull(5000) {
@@ -13,3 +15,51 @@ suspend fun hasBonusPaulScore(): Boolean = withTimeoutOrNull(5000) {
         mayor.getAsJsonArray("perks")?.any { it.asJsonObject.get("name")?.asString == "EZPZ" } == true
     } else false
 } == true
+
+suspend fun sendDataToServer(body: String, url: String = "http://localhost:3002/cga/users" ): String = withTimeoutOrNull(5000) {
+    return@withTimeoutOrNull try {
+        val connection = withContext(Dispatchers.IO) {
+            URL(url).openConnection()
+        } as HttpURLConnection
+        connection.requestMethod = "POST"
+        connection.doOutput = true
+        connection.setRequestProperty("Content-Type", "application/json")
+
+        val writer = OutputStreamWriter(connection.outputStream)
+        withContext(Dispatchers.IO) {
+            writer.write(body)
+        }
+        withContext(Dispatchers.IO) {
+            writer.flush()
+        }
+
+        val responseCode = connection.responseCode
+        val inputStream = if (responseCode in 200..299) connection.inputStream else connection.errorStream
+        val response = inputStream.bufferedReader().use { it.readText() }
+        connection.disconnect()
+
+        response
+    } catch (_: Exception) { "" }
+} ?: ""
+
+suspend fun getDataFromServer(url: String = "http://localhost:3002/cga/users"): String {
+    return withTimeoutOrNull(10000) {
+        try {
+            val connection = withContext(Dispatchers.IO) {
+                URL(url).openConnection()
+            } as HttpURLConnection
+            connection.requestMethod = "GET"
+
+            val responseCode = connection.responseCode
+//            if (DevPlayers.isDev) println("Response Code: $responseCode")
+            if (responseCode != 200) return@withTimeoutOrNull ""
+            val inputStream = connection.inputStream
+            val response = inputStream.bufferedReader().use { it.readText() }
+//            if (DevPlayers.isDev) println("Response: $response")
+
+            connection.disconnect()
+
+            response
+        } catch (_: Exception) { "" }
+    } ?: ""
+}
