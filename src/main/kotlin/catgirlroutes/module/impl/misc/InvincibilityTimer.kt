e989c010d225a19affdfc47182706a861d8b539c
pirog <mihail.larin00@gmail.com>
2024-12-30 00:19:17 +0300

    InvincibilityTimer cleanup

--- src/main/kotlin/catgirlroutes/module/impl/misc/InvincibilityTimer.kt
+++ src/main/kotlin/catgirlroutes/module/impl/misc/InvincibilityTimer.kt
@@ -28,13 +28,13 @@ object InvincibilityTimer : Module(
     @SubscribeEvent
     fun onChat(event: ClientChatReceivedEvent) {
         val msg = event.message.unformattedText
+
         when {
-            msg.contains("Second Wind Activated! Your Spirit Mask saved your life!") -> spiritTicks = (600 * getMageCooldownMultiplier()).toInt()
-            msg.contains("Bonzo's Mask saved your life!") -> bonzoTicks = (7200 - (cataLevel.value.toInt() * 72) * getMageCooldownMultiplier()).toInt()
-            msg.contains("Your Phoenix Pet saved you from certain death!") -> phoenixTicks = (1200)
-            msg.contains("You summoned your") -> phoenix = if (msg.contains("Phoenix")) true else false
-            msg.contains("Autopet equipped your") -> phoenix = if (msg.contains("Phoenix")) true else false
-            msg.contains("You despawned your Phoenix!") -> phoenix = false
+            "Second Wind Activated! Your Spirit Mask saved your life!" in msg -> spiritTicks = (600 * getMageCooldownMultiplier()).toInt()
+            "Bonzo's Mask saved your life!" in msg -> bonzoTicks = ((7200 - cataLevel.value.toInt() * 72) * getMageCooldownMultiplier()).toInt()
+            "Your Phoenix Pet saved you from certain death!" in msg -> phoenixTicks = 1200
+            "You summoned your" in msg || "Autopet equipped your" in msg -> phoenix = msg.contains("Phoenix")
+            "You despawned your Phoenix!" in msg -> phoenix = false
         }
     }
 
@@ -53,15 +53,28 @@ object InvincibilityTimer : Module(
     ) {
         override fun renderHud() {
             val name = mc.thePlayer?.inventory?.armorInventory?.get(3)?.displayName
-            val offset = if (name?.contains("Bonzo") == true) 10.0 else if (name?.contains("Spirit") == true) 20.0 else 10000.0 //schizo way to not render indictator on screen
-            val bonzoReady = if (bonzoTicks <= 0) "Bonzo: ${if (phoenix && offset == 10.0 && phoenixTicks < 0.0) "§c" else "§a"}✔" else "Bonzo: §c${(bonzoTicks / 20.0)}"
-            val spiritReady = if (spiritTicks <= 0) "Spirit: ${if (phoenix && offset == 20.0 && phoenixTicks < 0.0) "§c" else "§a"}✔" else "Spirit: §c${(spiritTicks / 20.0)}"
-            val phoenixReady = if (phoenixTicks <= 0) "Phoenix: ${if (phoenix && ((offset == 20.0 && spiritTicks < 0.0) || (offset == 10.0 && bonzoTicks < 0.0))) "§c" else "§a"}✔" else "Phoenix: §c${(phoenixTicks / 20.0)}"
-            mc.fontRendererObj.drawStringWithShadow(bonzoReady, 0.0F, 10.0f, java.awt.Color.PINK.rgb)
-            mc.fontRendererObj.drawStringWithShadow(spiritReady, 0.0F, 20.0f, java.awt.Color.PINK.rgb)
-            mc.fontRendererObj.drawStringWithShadow(phoenixReady, 0.0F, 30.0f, java.awt.Color.PINK.rgb)
+            val offset = when {
+                name?.contains("Bonzo") == true -> 10.0
+                name?.contains("Spirit") == true -> 20.0
+                else -> 10000.0  // some shit to hide some shit
+            }
+
+            val bonzoReady = stupid("Bonzo", bonzoTicks, phoenix && offset == 10.0 && phoenixTicks < 0.0)
+            val spiritReady = stupid("Spirit", spiritTicks, phoenix && offset == 20.0 && phoenixTicks < 0.0)
+            val phoenixReady = stupid("Phoenix", phoenixTicks, phoenix && ((offset == 20.0 && spiritTicks < 0.0) || (offset == 10.0 && bonzoTicks < 0.0)))
+
+            mc.fontRendererObj.drawStringWithShadow(bonzoReady, 0.0F, 10.0f, Color.PINK.rgb)
+            mc.fontRendererObj.drawStringWithShadow(spiritReady, 0.0F, 20.0f, Color.PINK.rgb)
+            mc.fontRendererObj.drawStringWithShadow(phoenixReady, 0.0F, 30.0f, Color.PINK.rgb)
+
             renderRect(-6.0, offset + 2.0, 3.0, 3.0, Color.PINK)
             if (phoenix) renderRect(-6.0, 32.0, 3.0, 3.0, Color.PINK)
+        }
+
+        private fun stupid(name: String, ticks: Int, highlight: Boolean): String {  // stupid thing for colour highlight
+            val c = if (ticks < 0 && highlight) "§c" else if (ticks <= 0) "§a" else "§c"
+            val time = if (ticks <= 0) "✔" else "%.1f".format(ticks / 20.0)
+            return "$name: $c$time"
         }
     }
 }
