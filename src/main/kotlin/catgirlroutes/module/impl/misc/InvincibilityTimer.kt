a0c0fe043eb4794c14daf748fdd371b2c06f712a
pirog <mihail.larin00@gmail.com>
2025-01-05 12:34:15 +0300

    Fix InvincibilityTimer shit

--- src/main/kotlin/catgirlroutes/module/impl/misc/InvincibilityTimer.kt
+++ src/main/kotlin/catgirlroutes/module/impl/misc/InvincibilityTimer.kt
@@ -63,21 +63,21 @@ object InvincibilityTimer : Module(
 
             val name = mc.thePlayer?.inventory?.armorInventory?.get(3)?.displayName
             val offset = when {
-                name?.contains("Bonzo") == true -> 10.0
-                name?.contains("Spirit") == true -> 20.0
+                name?.contains("Bonzo") == true -> 0.0
+                name?.contains("Spirit") == true -> 10.0
                 else -> 10000.0  // some shit to hide some shit
             }
 
-            val bonzoReady = stupid("Bonzo", bonzoTicks, phoenix && offset == 10.0 && phoenixTicks < 0.0)
-            val spiritReady = stupid("Spirit", spiritTicks, phoenix && offset == 20.0 && phoenixTicks < 0.0)
-            val phoenixReady = stupid("Phoenix", phoenixTicks, phoenix && ((offset == 20.0 && spiritTicks < 0.0) || (offset == 10.0 && bonzoTicks < 0.0)))
+            val bonzoReady = stupid("Bonzo", bonzoTicks, phoenix && offset == 0.0 && phoenixTicks < 0.0)
+            val spiritReady = stupid("Spirit", spiritTicks, phoenix && offset == 10.0 && phoenixTicks < 0.0)
+            val phoenixReady = stupid("Phoenix", phoenixTicks, phoenix && ((offset == 10.0 && spiritTicks < 0.0) || (offset == 0.0 && bonzoTicks < 0.0)))
 
             mc.fontRendererObj.drawStringWithShadow(bonzoReady, 6.0F, 0.0f, Color.PINK.rgb)
             mc.fontRendererObj.drawStringWithShadow(spiritReady, 6.0F, 10.0f, Color.PINK.rgb)
             mc.fontRendererObj.drawStringWithShadow(phoenixReady, 6.0F, 20.0f, Color.PINK.rgb)
 
             renderRect(0.0, offset + 2.0, 3.0, 3.0, Color.PINK)
-            if (phoenix) renderRect(0.0, 32.0, 3.0, 3.0, Color.PINK)
+            if (phoenix) renderRect(0.0, 22.0, 3.0, 3.0, Color.PINK)
         }
 
         private fun stupid(name: String, ticks: Int, highlight: Boolean): String {  // stupid thing for colour highlight
