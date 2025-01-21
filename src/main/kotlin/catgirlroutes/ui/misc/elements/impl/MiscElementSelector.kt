d663d214d309979b51a47c4367fda9d9b6208f89
pirog <mihail.larin00@gmail.com>
2025-01-21 15:39:43 +0300

    Add (MiscElementSelector) optionsPerRow with horizontal rendering and stop selecting first option if index is -1,

--- src/main/kotlin/catgirlroutes/ui/misc/elements/impl/MiscElementSelector.kt
+++ src/main/kotlin/catgirlroutes/ui/misc/elements/impl/MiscElementSelector.kt
@@ -4,12 +4,9 @@ import catgirlroutes.ui.clickgui.util.ColorUtil
 import catgirlroutes.ui.clickgui.util.FontUtil
 import catgirlroutes.ui.clickgui.util.FontUtil.capitalizeOnlyFirst
 import catgirlroutes.ui.misc.elements.MiscElement
-import catgirlroutes.utils.ChatUtils.debugMessage
 import catgirlroutes.utils.render.HUDRenderUtils
-import net.minecraft.util.MathHelper
 import java.awt.Color
 import java.util.*
-import kotlin.collections.ArrayList
 
 /**
  * @see StringSelectorSetting
@@ -20,10 +17,12 @@ class MiscElementSelector(
     var options: ArrayList<String>,
     x: Double = 0.0,
     y: Double = 0.0,
-    private val vertical: Boolean = true,
     width: Double = 80.0,
     height: Double = 20.0,
     var thickness: Double = 2.0,
+    var radius: Double = 5.0,
+    private val vertical: Boolean = true,
+    val optionsPerRow: Int = 999,
     val gap: Int = 5
 ) : MiscElement(x, y, width, height) {
 
@@ -33,16 +32,21 @@ class MiscElementSelector(
         set(newVal) {
             field = if (newVal > options.size - 1)  0 else if ( newVal < 0) options.size - 1 else newVal
         }
+
     var selected: String
-        set(value) {
-            index = optionIndex(value)
-        }
         get() {
-            return options[index]
+            return if (index == -1) this.defaultSelected
+            else options[index]
+        }
+        set(value) {
+            val newIndex = optionIndex(value)
+            if (newIndex != -1) {
+                index = newIndex
+            }
         }
 
     private fun optionIndex(string: String): Int {
-        return MathHelper.clamp_int(this.options.map { it.lowercase() }.indexOf(string.lowercase()), 0, options.size - 1)
+        return this.options.map { it.lowercase() }.indexOf(string.lowercase())
     }
 
     fun isSelected(string: String): Boolean {
@@ -53,7 +57,7 @@ class MiscElementSelector(
     override fun render(mouseX: Int, mouseY: Int) { // todo add some indicator it's a selector
         if (vertical) {
             HUDRenderUtils.drawRoundedBorderedRect(
-                this.x, this.y, this.width, if (extended) this.height * (this.options.size + 1) else this.height, 5.0, this.thickness,
+                this.x, this.y, this.width, if (extended) this.height * (this.options.size + 1) else this.height, this.radius, this.thickness,
                 Color(ColorUtil.elementColor), if (extended || isHovered(mouseX, mouseY)) ColorUtil.clickGUIColor else Color(ColorUtil.outlineColor)
             )
             FontUtil.drawTotalCenteredString(this.selected, x + this.width / 2.0, y + this.height / 2.0)
@@ -62,7 +66,7 @@ class MiscElementSelector(
             this.options.forEachIndexed { i, option ->
 
                 if (this.isSelected(option) || this.isHovered(mouseX, mouseY, yOff = this.height.toInt() * (i + 1))) {
-                    HUDRenderUtils.drawRoundedRect(this.x, this.y + (this.height * (i + 1)), this.width, this.height, 5.0, ColorUtil.clickGUIColor)
+                    HUDRenderUtils.drawRoundedRect(this.x, this.y + (this.height * (i + 1)), this.width, this.height, this.radius, ColorUtil.clickGUIColor)
                 }
 
                 val optionName = option.substring(0, 1).uppercase(Locale.getDefault()) + option.substring(1, option.length)
@@ -72,17 +76,22 @@ class MiscElementSelector(
         }
 
         this.options.forEachIndexed { i, option ->
-            val xPos = this.x + i * this.width + i * this.gap
+            val row = i / 5
+            val column = i % 5
+
+            val xPos = this.x + column * (this.width + this.gap)
+            val yPos = this.y + row * (this.height + this.gap)
+
             HUDRenderUtils.drawRoundedBorderedRect(
-                xPos, this.y, this.width, this.height, 5.0, this.thickness,
+                xPos, yPos, this.width, this.height, 5.0, this.thickness,
                 Color(ColorUtil.elementColor), if (this.isSelected(option)) ColorUtil.clickGUIColor else Color(ColorUtil.outlineColor)
             )
 
-            if (this.isSelected(option) || this.isHovered(mouseX, mouseY, xOff = (xPos - this.x).toInt())) {
-                HUDRenderUtils.drawOutlinedRectBorder(xPos, this.y, this.width, this.height, 5.0, this.thickness, ColorUtil.clickGUIColor)
+            if (this.isSelected(option) || this.isHovered(mouseX, mouseY, xOff = (xPos - this.x).toInt(), yOff = (yPos - this.y).toInt())) {
+                HUDRenderUtils.drawOutlinedRectBorder(xPos, yPos, this.width, this.height, this.radius, this.thickness, ColorUtil.clickGUIColor)
             }
 
-            FontUtil.drawTotalCenteredString(option.capitalizeOnlyFirst(), this.x + this.width / 2.0 + xPos - this.x, this.y + height / 2.0)
+            FontUtil.drawTotalCenteredString(option.capitalizeOnlyFirst(), xPos + this.width / 2.0, yPos + this.height / 2.0)
         }
     }
 
@@ -96,14 +105,23 @@ class MiscElementSelector(
             if (!extended && this.vertical) return false
 
             this.options.forEachIndexed { i, option ->
-                val isOptionHovered = if (this.vertical)
-                    this.isHovered(mouseX, mouseY, yOff = this.height.toInt() * (i + 1))
-                else
-                    this.isHovered(mouseX, mouseY, xOff = i * this.width.toInt() + i * this.gap)
+                if (this.vertical) {
+                    val yOff = i * (this.height.toInt() + this.gap)
+                    if (this.isHovered(mouseX, mouseY, yOff = yOff)) {
+                        this.selected = option
+                        return true
+                    }
+                } else {
+                    val row = i / optionsPerRow
+                    val column = i % optionsPerRow
 
-                if (isOptionHovered) {
-                    this.selected = option
-                    return true
+                    val xOff = column * (this.width.toInt() + this.gap)
+                    val yOff = row * (this.height.toInt() + this.gap)
+
+                    if (this.isHovered(mouseX, mouseY, xOff = xOff, yOff = yOff)) {
+                        this.selected = option
+                        return true
+                    }
                 }
             }
         } else if (mouseButton == 1 && isHovered(mouseX, mouseY) && this.vertical) {
