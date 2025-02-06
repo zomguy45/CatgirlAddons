92032d2b8a2e965e43b9e17f411f4c538d67d28b
pirog <mihail.larin00@gmail.com>
2025-02-06 03:25:12 +0300

    Add (FontUtil) scale support

--- src/main/kotlin/catgirlroutes/ui/clickgui/util/FontUtil.kt
+++ src/main/kotlin/catgirlroutes/ui/clickgui/util/FontUtil.kt
@@ -19,8 +19,8 @@ object FontUtil {
         fontRenderer = mc.fontRendererObj
     }
 
-    fun getStringWidth(text: String?): Int {
-        return fontRenderer!!.getStringWidth(StringUtils.stripControlCodes(text))
+    fun getStringWidth(text: String?, scale: Double = 1.0): Int {
+        return (fontRenderer!!.getStringWidth(StringUtils.stripControlCodes(text)) * scale).toInt()
     }
 
     fun getSplitHeight(text: String, wrapWidth: Int): Int {
@@ -34,37 +34,52 @@ object FontUtil {
     val fontHeight: Int
         get() = fontRenderer!!.FONT_HEIGHT
 
-    fun drawString(text: String, x: Double, y: Double, color: Int = ColorUtil.textcolor) {
-        drawString(text, x.toInt(), y.toInt(), color)
+    fun getScaledFontHeight(scale: Double = 1.0): Int {
+        return (fontHeight * scale).toInt()
     }
 
-    fun drawString(text: String, x: Int, y: Int, color: Int = ColorUtil.textcolor) {
-        fontRenderer!!.drawString(text, x, y, color)
+    fun drawString(text: String, x: Double, y: Double, color: Int = ColorUtil.textcolor, scale: Double = 1.0) {
+        drawString(text, x.toInt(), y.toInt(), color, scale)
     }
 
-    fun drawStringWithShadow(text: String, x: Double, y: Double, color: Int = ColorUtil.textcolor) {
-        fontRenderer?.drawStringWithShadow(text, x.toFloat(), y.toFloat(), color)
+    fun drawString(text: String, x: Int, y: Int, color: Int = ColorUtil.textcolor, scale: Double = 1.0) {
+        GlStateManager.pushMatrix()
+        GlStateManager.translate(x.toDouble(), y.toDouble(), 0.0)
+        GlStateManager.scale(scale.toFloat(), scale.toFloat(), 1.0f)
+        fontRenderer!!.drawString(text, 0, 0, color)
+        GlStateManager.popMatrix()
     }
 
-    fun drawCenteredString(text: String, x: Double, y: Double, color: Int = ColorUtil.textcolor) {
-        drawString(text, x - fontRenderer!!.getStringWidth(text) / 2, y, color)
+    fun drawStringWithShadow(text: String, x: Double, y: Double, color: Int = ColorUtil.textcolor, scale: Double = 1.0) {
+        GlStateManager.pushMatrix()
+        GlStateManager.translate(x.toDouble(), y.toDouble(), 0.0)
+        GlStateManager.scale(scale.toFloat(), scale.toFloat(), 1.0f)
+        fontRenderer?.drawStringWithShadow(text, 0f, 0f, color)
+        GlStateManager.popMatrix()
     }
 
-    fun drawCenteredStringWithShadow(text: String, x: Double, y: Double, color: Int = ColorUtil.textcolor) {
-        drawStringWithShadow(text, x - fontRenderer!!.getStringWidth(text) / 2, y, color)
+    fun drawCenteredString(text: String, x: Double, y: Double, color: Int = ColorUtil.textcolor, scale: Double = 1.0) {
+        val scaledX = x - (fontRenderer!!.getStringWidth(text) * scale) / 2
+        val scaledY = y
+        drawString(text, scaledX, scaledY, color, scale)
     }
 
-    fun drawTotalCenteredString(text: String, x: Double, y: Double, color: Int = ColorUtil.textcolor) {
-        drawString(text, x - fontRenderer!!.getStringWidth(text) / 2, y - fontRenderer!!.FONT_HEIGHT / 2, color)
+    fun drawCenteredStringWithShadow(text: String, x: Double, y: Double, color: Int = ColorUtil.textcolor, scale: Double = 1.0) {
+        val scaledX = x - (fontRenderer!!.getStringWidth(text) * scale) / 2
+        val scaledY = y
+        drawStringWithShadow(text, scaledX, scaledY, color, scale)
     }
 
-    fun drawTotalCenteredStringWithShadow(text: String, x: Double, y: Double, color: Int = ColorUtil.textcolor) {
-        drawStringWithShadow(
-            text,
-            x - fontRenderer!!.getStringWidth(text) / 2,
-            y - fontRenderer!!.FONT_HEIGHT / 2f,
-            color
-        )
+    fun drawTotalCenteredString(text: String, x: Double, y: Double, color: Int = ColorUtil.textcolor, scale: Double = 1.0) {
+        val scaledX = x - (fontRenderer!!.getStringWidth(text) * scale) / 2
+        val scaledY = y - (fontRenderer!!.FONT_HEIGHT * scale) / 2
+        drawString(text, scaledX, scaledY, color, scale)
+    }
+
+    fun drawTotalCenteredStringWithShadow(text: String, x: Double, y: Double, color: Int = ColorUtil.textcolor, scale: Double = 1.0) {
+        val scaledX = x - (fontRenderer!!.getStringWidth(text) * scale) / 2
+        val scaledY = y - (fontRenderer!!.FONT_HEIGHT * scale) / 2
+        drawStringWithShadow(text, scaledX, scaledY, color, scale)
     }
 
     /**
