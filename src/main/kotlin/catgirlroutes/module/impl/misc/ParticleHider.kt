1082a2179ff1fce82fdcd8a23230a24740821267
pigeonlover1998 <200670258+pigeonlover1998@users.noreply.github.com>
2025-03-30 05:22:24 +0300

    Fix (ParticleHider) breaking dragprio

--- src/main/kotlin/catgirlroutes/module/impl/misc/ParticleHider.kt
+++ src/main/kotlin/catgirlroutes/module/impl/misc/ParticleHider.kt
@@ -3,6 +3,8 @@ package catgirlroutes.module.impl.misc
 import catgirlroutes.events.impl.PacketReceiveEvent
 import catgirlroutes.module.Category
 import catgirlroutes.module.Module
+import catgirlroutes.utils.dungeon.DungeonUtils
+import catgirlroutes.utils.dungeon.M7Phases
 import net.minecraft.network.play.server.S2APacketParticles
 import net.minecraftforge.fml.common.eventhandler.EventPriority
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
@@ -11,6 +13,6 @@ object ParticleHider : Module(
     "Particle hider",
     Category.MISC
 ) {
-    @SubscribeEvent(priority = EventPriority.LOW)
-    fun onPacket(event: PacketReceiveEvent) { event.isCanceled = event.packet is S2APacketParticles }
+    @SubscribeEvent
+    fun onPacket(event: PacketReceiveEvent) { event.isCanceled = event.packet is S2APacketParticles && DungeonUtils.getF7Phase() != M7Phases.P5 }
 }
