4463f7a53c42313421166017e55ab58f11173b2b
TurtleOnFire2 <ijon.lorenz@gmail.com>
2025-04-23 23:48:10 +0200

    Made watcher kil notif a hud

--- src/main/kotlin/catgirlroutes/module/impl/dungeons/Watcher.kt
+++ src/main/kotlin/catgirlroutes/module/impl/dungeons/Watcher.kt
@@ -5,9 +5,13 @@ import catgirlroutes.module.Category
 import catgirlroutes.module.Module
 import catgirlroutes.module.settings.RegisterHudElement
 import catgirlroutes.module.settings.impl.BooleanSetting
+import catgirlroutes.module.settings.impl.HudSetting
 import catgirlroutes.module.settings.impl.NumberSetting
+import catgirlroutes.ui.clickgui.util.FontUtil.drawStringWithShadow
+import catgirlroutes.utils.ClientListener.scheduleTask
 import catgirlroutes.utils.render.WorldRenderUtils.displayTitle
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
+import java.awt.Color
 
 object Watcher : Module(
     name = "Watcher",
@@ -15,6 +19,13 @@ object Watcher : Module(
 ) {
     val killTitle = BooleanSetting("Display title", false)
     val displayTime = NumberSetting("Display time", 40.0, 20.0, 100.0)
+    val hud by HudSetting{
+        size("KILL NOW!")
+        visibleIf { title }
+        render { drawStringWithShadow("KILL NOW!", 0.0, 0.0, Color.PINK.rgb) }
+    }
+
+    private var title = false
 
     init {
         this.addSettings(killTitle, displayTime)
@@ -23,7 +34,8 @@ object Watcher : Module(
     @SubscribeEvent
     fun onEntityLeave(event: EntityRemovedEvent) {
         if (event.entity.name.contains("Let's see how you can handle this.") && killTitle.value) {
-            displayTitle("KILL NOW!", displayTime.value.toInt())
+            title = true
+            scheduleTask(displayTime.value.toInt()) { title = false }
         }
     }
 }
