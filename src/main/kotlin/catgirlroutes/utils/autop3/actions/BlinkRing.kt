a655933a2659898efd540418d87f01821dce06c9
zomguy45 <reed.ennis1234@gmail.com>
2025-08-14 21:22:51 -0600

    Update BlinkRing.kt

--- src/main/kotlin/catgirlroutes/utils/autop3/actions/BlinkRing.kt
+++ src/main/kotlin/catgirlroutes/utils/autop3/actions/BlinkRing.kt
@@ -10,43 +10,29 @@ import catgirlroutes.utils.autop3.RingsManager.blinkCd
 import catgirlroutes.utils.autop3.RingsManager.blinkEditMode
 import catgirlroutes.utils.customtriggers.TypeName
 import net.minecraft.network.play.client.C03PacketPlayer
-import net.minecraftforge.common.MinecraftForge
-import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
-import net.minecraftforge.fml.common.gameevent.TickEvent
 
 @TypeName("blink")
 class BlinkRing(var packets: MutableList<Blink.BlinkC06> = mutableListOf()) : RingAction() {
-    private var queuedBlink = false
-
-    init {
-        MinecraftForge.EVENT_BUS.register(this)
-    }
-
+//    override val description: String = "teleports the player"
     override fun execute(ring: Ring) {
         if (blinkEditMode) return
-        if (packets.isEmpty()) return modMessage("Not enough packets.")
+        if (packets.size == 0) return modMessage("Not enough packets.")
         if (packetArray >= packets.size && !blinkCd) {
-            queuedBlink = true
-        }
-    }
-
-    @SubscribeEvent
-    fun onTick(event: TickEvent.ClientTickEvent) {
-        if (event.phase == TickEvent.Phase.START && queuedBlink) {
-            queuedBlink = false
-            blinkCd = true
-            packets.forEach { packet ->
-                mc.netHandler.networkManager.sendPacket(
-                    C03PacketPlayer.C04PacketPlayerPosition(
-                        packet.x,
-                        packet.y,
-                        packet.z,
-                        packet.onGround
+            scheduleTask(0) {
+                blinkCd = true
+                packets.forEach { packet -> // todo make a util
+                    mc.netHandler.networkManager.sendPacket(
+                        C03PacketPlayer.C04PacketPlayerPosition(
+                            packet.x,
+                            packet.y,
+                            packet.z,
+                            packet.onGround
+                        )
                     )
-                )
-                packetArray -= 1
+                    packetArray -= 1
+                }
+                mc.thePlayer.setPosition(packets.last().x, packets.last().y, packets.last().z)
             }
-            mc.thePlayer.setPosition(packets.last().x, packets.last().y, packets.last().z)
             scheduleTask(1) {
                 blinkCd = false
             }
